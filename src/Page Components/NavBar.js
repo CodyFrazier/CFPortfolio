8663d92f193ae5a506c0d3634ea0f596124f26c0
CodyFrazier <codyfrazier@mac.com>
2022-06-03 18:33:50 -0700

    Restyled NavBar and gave it some flair

--- src/Page Components/NavBar.js
+++ src/Page Components/NavBar.js
@@ -22,14 +22,14 @@ const NavBar = ({ bp, setError }) => {
 
     return (
         <div className = { `${ bp.isLarge ? '' : 'z1 widthundred' }` }>
-            { bp.isLarge === false && <input type = 'button' className = 'padHalf widthundred bgAO noBorder colorOW' onClick = { () => { setDisplayNav(!displayNav) } } value = { `${ displayNav ? 'Collapse' : 'Menu' }` } /> }
-            { (bp.isLarge === true || displayNav === true) && <div className = { `${ bp.isLarge ? 'rowNW spaceAroundRow' : 'columnNW' } widthundred bgAO` }>
-                <Link to = '/' onClick = { () => menuDisplay() } className = { `${ bp.isLarge ? '' : 'topBorderDG' } padHalf undecorated` }>Home</Link>
-                <Link to = '/projects/' onClick = { () => menuDisplay() } className = { `${ bp.isLarge ? '' : 'topBorderDG' } padHalf undecorated` }>Projects</Link>
-                <Link to = '/contact/' onClick = { () => menuDisplay() } className = { `${ bp.isLarge ? '' : 'topBorderDG' } padHalf undecorated` }>Contact Information</Link>
-                <Link to = '/education/' onClick = { () => menuDisplay() } className = { `${ bp.isLarge ? '' : 'topBorderDG' } padHalf undecorated` }>Education</Link>
-                <Link to = '/workhistory/' onClick = { () => menuDisplay() } className = { `${ bp.isLarge ? '' : 'topBorderDG' } padHalf undecorated` }>Work History</Link>
-                <Link to = '/bio/' onClick = { () => menuDisplay() } className = { `${ bp.isLarge ? '' : 'topBorderDG' } padHalf undecorated` }>Bio</Link>
+            { bp.isLarge === false && <input type = 'button' className = 'padHalf widthundred bgPGRGBA borderAO bottomLeft5 bottomRight5 colorOW frosted' onClick = { () => { setDisplayNav(!displayNav) } } value = { `${ displayNav ? 'Collapse' : 'Menu' }` } /> }
+            { (bp.isLarge === true || displayNav === true) && <div className = { `${ bp.isLarge ? 'rowNW spaceAroundRow borderAO bgPGRGBA' : 'columnNW' }` }>
+                <Link to = '/' onClick = { () => menuDisplay() } className = { `${ bp.isLarge ? '' : 'borderAO marginHalf border20' } menuConstants` }>Home</Link>
+                <Link to = '/projects/' onClick = { () => menuDisplay() } className = { `${ bp.isLarge ? '' : 'borderAO marginHalf rightMargin1 border20' } menuConstants` }>Projects</Link>
+                <Link to = '/contact/' onClick = { () => menuDisplay() } className = { `${ bp.isLarge ? '' : 'borderAO marginHalf rightMargin2 border20' } menuConstants` }>Contact Information</Link>
+                <Link to = '/education/' onClick = { () => menuDisplay() } className = { `${ bp.isLarge ? '' : 'borderAO marginHalf rightMargin4 border20' } menuConstants` }>Education</Link>
+                <Link to = '/workhistory/' onClick = { () => menuDisplay() } className = { `${ bp.isLarge ? '' : 'borderAO marginHalf rightMargin8 border20' } menuConstants` }>Work History</Link>
+                <Link to = '/bio/' onClick = { () => menuDisplay() } className = { `${ bp.isLarge ? '' : 'borderAO marginHalf rightMargin14 border20' } menuConstants` }>Bio</Link>
             </div> }
         </div>
     )
